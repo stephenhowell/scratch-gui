b4d5d969705e36a55dffab9d6b88187703984178
Stephen Howell <stephenhowell@outlook.com>
2025-07-14 11:31:09 +0100

    feat: change title from default to show it's body blocks not scratch 3.0 vanilla

--- webpack.config.js
+++ webpack.config.js
@@ -126,7 +126,7 @@ const buildConfig = baseConfig.clone()
         ...commonHtmlWebpackPluginOptions,
         chunks: ['gui'],
         template: 'src/playground/index.ejs',
-        title: 'Scratch 3.0 GUI'
+        title: 'Body Blocks for Scratch 3.0'
     }))
     .addPlugin(new HtmlWebpackPlugin({
         ...commonHtmlWebpackPluginOptions,
